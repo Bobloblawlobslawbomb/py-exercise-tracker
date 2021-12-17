29cf7492436ad7bc4a74b45aae0fa1f0ad922e81
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-12-16 16:18:59 -0800

    make api call/print out response

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ load_dotenv(dotenv_path)
 
 NUTRITIONIX_APP_ID = os.environ.get('NUTRITIONIX_APP_ID')
 NUTRITIONIX_API_KEY = os.environ.get('NUTRITIONIX_API_KEY')
+
+gender = os.environ.get('GENDER')
+weight = os.environ.get('WEIGHT')
+height = os.environ.get('HEIGHT')
+age = os.environ.get('AGE')
+
+nutritionix_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+nutri_headers = {
+    'x-app-id': NUTRITIONIX_APP_ID,
+    'x-app-key': NUTRITIONIX_API_KEY,
+    'x-remote-user-id': "0",
+}
+
+user_input = input("What did you do today?: ")
+
+exercise_data = {
+    'query': user_input,
+    'gender': gender,
+    'weight_kg': weight,
+    'height_cm': height,
+    'age': age
+}
+
+nutrition_response = requests.post(
+    url=nutritionix_url, headers=nutri_headers, json=exercise_data)
+
+print(nutrition_response.text)
+print(nutrition_response.json())
